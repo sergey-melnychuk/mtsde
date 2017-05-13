b4b67a80a029f5da4a47550a2f30076c3c851a3e
sergey-melnychuk <serg.meln@gmail.com>
2017-05-13 19:27:18 +0200

    Add system-wide unique id for a Movie entity

--- src/main/scala/edu/mtsde/domain/Movie.scala
+++ src/main/scala/edu/mtsde/domain/Movie.scala
@@ -1,3 +1,5 @@
 package edu.mtsde.domain
 
-case class Movie(imdbId: String, title: String, screenId: String, availableSeats: Int, tickets: List[Ticket])
+case class Movie(imdbId: String, title: String, screenId: String, availableSeats: Int, tickets: List[Ticket]) {
+  val id = imdbId + "." + screenId
+}
